9de8cff5ee54f299640d4d793855b0bae984db4b
Vishal Dharmawat <vishal.dharmawat@gmail.com>
2013-04-14 12:18:04 +0530

    Initial Commit

---
+++ BusinessHelper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BusinessHelper
+//
+//  Created by Vishal Dharmawat on 14/04/13.
+//  Copyright (c) 2013 Vishal Dharmawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
